74e71beb2f6a42897067b9a7c2658438180cef7e
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-04-25 22:49:10 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Math Practice Game
+
+# Introduction
+This project is developed by Shehan Abeyrathne to fullfil `Kinnet - Angular Developer Assignment`.
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,130 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  // define variables
+  number_one: number = 0;
+  number_two: number = 0;
+  user_input: any = {
+    answer: null
+  };
+  @ViewChild("userInput") userInput: any;
+  correctAnswer: boolean = false;
+  startTime: Date;
+  ellapsedTime = '00:00';
+  timer: any = null;
+  diff: number;
+  avarageTime: number;
+  totalTime: number = 0;
+  questionNumber: number = 1;
+  showAvarageTime = '00:00';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // init values
+    this.generateRandomNumber();
+    this.startTime = new Date();
+    this.ellapsedTime = '00:00';
+    this.startTimer();
+  }
+
+  ngAfterViewInit() {
+    this.userInput.nativeElement.focus();
+  }
+
+  // method to generate random number 0-9
+  generateRandomNumber() {
+    this.number_one = Math.floor(Math.random() * 10);
+    this.number_two = Math.floor(Math.random() * 10);
+  }
+
+  // method to calculate after press enter
+  cal() {
+    this.stopTimer();
+    var totalVal = this.number_one + this.number_two;
+    // correct answer
+    if (totalVal == this.user_input.answer) {
+      // show correct message
+      this.correctAnswer = true;
+      var showBtn = document.getElementById("showResponseButton");
+      if (showBtn != null) {
+        showBtn.click();
+      }
+      // clear numbers
+      this.generateRandomNumber();
+      // clear user input
+      this.user_input.answer = null;
+    }
+    // incorrect answer
+    else {
+      // show wrong message
+      this.correctAnswer = false;
+      var showBtn = document.getElementById("showResponseButton");
+      if (showBtn != null) {
+        showBtn.click();
+      }
+      // clear user input
+      this.user_input.answer = null;
+    }
+  }
+
+  // method to contuine the game
+  continueBtn() {
+    var closeBtn = document.getElementById("closeResponseButton");
+    if (closeBtn != null) {
+      closeBtn.click();
+    }
+    this.userInput.nativeElement.focus();
+    // increase question number
+    this.questionNumber = this.questionNumber + 1;
+    // re start timer again
+    this.startTimer();
+  }
+
+  // method to try again the game
+  tryAgainBtn(){
+    var closeBtn = document.getElementById("closeResponseButton");
+    if (closeBtn != null) {
+      closeBtn.click();
+    }
+    this.userInput.nativeElement.focus();
+    // re start timer again
+    this.startTimer();
+  }
+
+  // method to manage timer
+  tick() {
+    const now = new Date();
+    this.diff = (now.getTime() - this.startTime.getTime()) / 1000;
+    this.ellapsedTime = this.parseTime(this.diff);
+  }
+
+  parseTime(totalSeconds: number) {
+    let mins: string | number = Math.floor(totalSeconds / 60);
+    let secs: string | number = Math.round(totalSeconds % 60);
+    mins = (mins < 10 ? '0' : '') + mins;
+    secs = (secs < 10 ? '0' : '') + secs;
+    return `${mins}:${secs}`;
+  }
+
+  // method to stop timmer
+  stopTimer(){
+    this.totalTime = this.totalTime + this.diff;
+    this.avarageTime = this.totalTime / this.questionNumber;
+    this.showAvarageTime = this.parseTime(this.avarageTime);
+    clearInterval(this.timer);
+  }
+
+  // method to start timmer
+  startTimer(){
+    this.startTime = new Date();
+    this.timer = setInterval(() => { this.tick(); }, 1000);
+  }
+
+}
